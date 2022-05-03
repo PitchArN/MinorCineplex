ed40eb961a5d51cdbe61bcc2e47589697964d5b3
l2emilie <104815507+l2emilie@users.noreply.github.com>
2022-05-03 16:04:45 +0700

    Create Promotion1.php

---
+++ PointPromotion/Promotion1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="LoginButton">
+            <button>Login</button>
+        </div>
+        <hr>
+        <div class="PromotionBannerB">
+
+        </div>
+        <div class="PromotionDetail">
+            <div class="Detail">
+                <span>Detail</span>
+                <h3>50% 0ff</h3>
+                <p>offer ends after 5 days</p>
+            </div>
+        </div>
+    </body>
+</html>
