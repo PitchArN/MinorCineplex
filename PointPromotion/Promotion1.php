a21f58ccc804b8797ea58cf59dbe34a99c1948b3
l2emilie <104815507+l2emilie@users.noreply.github.com>
2022-05-04 14:09:25 +0700

    Update Promotion1.php

--- PointPromotion/Promotion1.php
+++ PointPromotion/Promotion1.php
@@ -4,11 +4,53 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <div class="LoginButton">
-            <button>Login</button>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+             <a class="navbar-brand" href="#">
+              <img src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
+                 Minor Cineplex
+             </a>
+           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+           <span class="navbar-toggler-icon"></span>
+             </button>
+             <div class="collapse navbar-collapse" id="navbarSupportedContent">
+             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+               <a class="nav-link active" aria-current="page" href="../Home/Home1.php">Home</a>
+               </li>
+          <li class="nav-item">
+              <a class="nav-link" href=""></a>
+         </li>
+          <li class="nav-item dropdown">
+           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Product
+           </a>
+           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="../Popcron/BuyPop.php">Popcorn</a></li>
+             <li><a class="dropdown-item" href="#">Another Products</a></li>
+             </ul>
+             </li>
+             <li class="nav-item">
+              <a class="nav-link disabled">Contact</a>
+             </li>
+             </ul>
+            <form class="d-flex">
+            <input class="form-control me-3" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success me-2" type="submit">Login</button>
+         <button class="btn btn-outline-success" type="submit">Signin</button>
+        </form>
+         </div>
         </div>
-        <hr>
+    </div>
+    </nav>
+
         <div class="PromotionBannerB">
+
         </div>
+        <div class="Detail">
+        <p>Detail </p>
+        <h3>Buy 1 Get 1 Free For 100 MC point</h3>
+        </div>
+
     </body>
 </html>
