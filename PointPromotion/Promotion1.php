1150920c21a9863389bcab552c1e57f20975c8d5
l2emilie <104815507+l2emilie@users.noreply.github.com>
2022-05-04 14:10:55 +0700

    Update Promotion1.php

--- PointPromotion/Promotion1.php
+++ PointPromotion/Promotion1.php
@@ -44,6 +44,31 @@
     </div>
     </nav>
 
+    <!--banner-->
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+  </div>
+  <center>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://asicsulb.org/corporate/images/connect/beachfront/2018-03/Movies-on-the-House.jpg" class="d-block w-75" alt="Civi war">
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.asicsulb.org/corporate/images/connect/beachfront/2019-01/movies-on-the-house.jpg" class="d-block w-75" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div></center>
+
         <div class="PromotionBannerB">
 
         </div>
