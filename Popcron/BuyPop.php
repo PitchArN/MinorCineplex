d23fd9391c93b639dcd0acbef3ca9f12000c5859
Tienlnwza <pichai.tien@mail.kmutt.ac.th>
2022-05-04 17:03:10 +0700

    แก้หน้าบ้าน

--- Popcron/BuyPop.php
+++ Popcron/BuyPop.php
@@ -9,9 +9,56 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!---------- Boothstrap ------->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
     </head>
     <body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="#">
+      <img src="https://static.nhentai.net/img/logo.090da3be7b51.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
+      Minor Cineplex
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href=""></a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Product
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <li><a class="dropdown-item" href="../Popcron/BuyPop.php">Popcorn</a></li>
+            <li><a class="dropdown-item" href="#">Another Products</a></li>
+            
+            <!--<li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#"></a></li>-->
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Contact</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-3" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success me-2" type="submit">Login</button>
+        <button class="btn btn-outline-success" type="submit">Signin</button>
+      </form>
+      </div>
+    </div>
+  </div>
+</nav>
+
+
+
     <center><div class="box">
         <h1>Pop Corn</h1>
         <p>Promotion</p>
@@ -29,7 +76,7 @@
 							$row = mysqli_fetch_row($result);
 							echo $row[0];
 						?>
-                        <input type="number" name = "Sweet" placeholder="quantity" min = "0" max = "<?php echo $row[0]; ?>">
+                        <center><input type="number" name = "Sweet" placeholder="quantity"  min = "0" max = "<?php echo $row[0]; ?>" font ></center>
                     </div>
                 </div>
                 </div>
@@ -47,7 +94,7 @@
 							$row = mysqli_fetch_row($result);
 							echo $row[0];
 						?>
-        <input type="number" name = "Salty" placeholder="quantity" min = "0" max = "<?php echo $row[0]; ?>">
+        <center><input type="number" name = "Salty" placeholder="quantity" min = "0" max = "<?php echo $row[0]; ?>"></center>
     </div>
 </div>
 </div>
@@ -64,11 +111,80 @@
 							$row = mysqli_fetch_row($result);
 							echo $row[0];
 						?>
-        <input type="number" name = "BQQ" placeholder="quantity" min = "0" max = "<?php echo $row[0]; ?>">
+<<<<<<< HEAD
+<<<<<<< HEAD
+        <center><input type="number" name = "BQQ" placeholder="quantity" min = "0" max = "<?php echo $row[0]; ?>"></center>
+        <center><q>Add all selected popcorn to the cart</q></center>
+        <center><input type = "submit" name = "AddToCart" value = "add to cart"></center>
+=======
+=======
+>>>>>>> b6e7c4feb9617a64f648ecbb3896c85495456c9d
+        <input type="number" name = "BBQ" placeholder="quantity" min = "0" max = "<?php echo $row[0]; ?>">
         <input type = "submit" name = "AddToCart" value = "add to cart">
+>>>>>>> b6e7c4feb9617a64f648ecbb3896c85495456c9d
     </div>
 </div>
 </div>
 </form>
+<!-- Contact -->
+
+<style> .bg{
+    background: rgba(0,0,0,0.6);
+    color: white;
+} 
+</style>
+
+<div class = "bg">
+  <div class="container">
+  <footer class="row row-cols-5 py-5 border-top">
+    <div class="col">
+      <a href="/" class="d-flex align-items-center mb-3 text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+      </a>
+      <p class="text-muted">© 2022</p>
+    </div>
+
+    <div class="col">
+
+    </div>
+
+    <div class="col" >
+      <h5>Section</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+      </ul>
+    </div>
+
+    <div class="col">
+      <h5>Section</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+      </ul>
+    </div>
+
+    <div class="col">
+      <h5>Section</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+      </ul>
+    </div>
+    <div class="d-flex justify-content-between py-4 my-4 border-top">
+      <p>© 2022 Minor Cineplex</p>
+    </div>
+  </footer>
+  </div>
+</div>
     </body>
 </html>
