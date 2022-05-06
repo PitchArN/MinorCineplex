978a4ebb2b7741800819435df6a769463c7e87ee
Kokkekokko <tawan1403@gmail.com>
2022-05-06 15:50:07 +0700

    Test UPDATE item stock

---
+++ Popcron/Send.php
@@ -0,0 +1,8 @@
+<html>
+<form action = "TEST Bil CON.php" method = "post" enctype = "multipart/form-data">
+				<input type = "hidden" name = "Sweet" value = "1">
+				<input type = "hidden" name = "Salty" value = "2">
+				<input type = "hidden" name = "BBQ" value = "3">
+                <input type = "submit" name = "Purchase" value = "Purchase">
+</form>
+</html>
