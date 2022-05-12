44d83d33fa2b32f40a56c45ba2d29a1899e1f0b8
l2emilie <104815507+l2emilie@users.noreply.github.com>
2022-05-12 22:11:35 +0700

    new

--- PointPromotion/ProFinish1.php
+++ PointPromotion/ProFinish1.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+    <title>MinorCineplex | PointPromotion</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="StyleCopy.scss">
@@ -24,13 +25,14 @@
         </h2>
         <p> 
             มา 3 จ่าย 2</br>
-            (โปรอย่าเผลอแดกทองเข้าไป)</br></br>
+            Point Used :</br>
+            Point Remain :</br>
         </p>
       </div>
     </div><!--End Invoice Mid-->
     <div id="bot">
 					<div id="legalcopy">
-						<p class="legal"><strong><a class="button" href="../Home/Home1.php">ชำระเสร็จสิ้น</a></strong>   
+						<p class="legal"><strong><a class="button" href="../Home/Home1.php">แลกแต้มสำเร็จ</a></strong>   
 						</p>
 					</div>
 
