ab40f8857ae9bc2e646f26907f32951a352e4c80
Hiro45346 <pittaya732@gmail.com>
2022-05-13 17:23:27 +0700

    decorate promotion_End

--- PointPromotion/Promotion_End.php
+++ PointPromotion/Promotion_End.php
@@ -4,26 +4,47 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Thank You</title>
+    <link rel ="stylesheet" href = "Promotion_End.css">
 </head>
 <body>
     
-
+<div class = "wrapper" align = "center">
+  <div id = "modalbox" class="modal">
+      <div class = "box">
 <div class="container">
       <div class="row-2  d-flex justify-content-center">
       <h1>
-        Ticket Purchased, Thank You
-      </h1><br>
+        Thank You
+      </h1>
+    </div>
+
+    <div id="mid">
+        </h2>
+        <p> 
+            มา 3 จ่าย 2</br>
+            Detail โปรโมชั่น โปรโมชั่น โปรโมชั่น โปรโมชั่น โปรโมชั่น โปรโมชั่น </br>
+            โปรโมชั่น โปรโมชั่น โปรโมชั่น โปรโมชั่น โปรโมชั่น โปรโมชั่น โปรโมชั่น </br>
+            โปรโมชั่น โปรโมชั่น โปรโมชั่น โปรโมชั่น โปรโมชั่น โปรโมชั่น โปรโมชั่น</br>
+        </p>
+            <h4>QRCODE<h4>
+        <p>
+            Point Used :</br>
+            Point Remain :</br>
+        </p>
+      </div>
     </div>
     <div class="row  d-flex justify-content-center">
       <label>CONFIRMATION CODE:</label><br><br>
-     <input type="password" name="" readonly value="<?php echo date("ymdsih",strtotime("now")); ?>" class = "textbox1">
+     <input type="text" name="" readonly value="<?php echo date("ymdsih",strtotime("now")); ?>" class = "textbox1">
     </div><br>
     <div class="row-1 d-flex justify-content-center">
       <h3>Use this confirmation code at the Cinema</h3>
     </div>
-    <a href = "HomeWback.php"><button class = "btn-2" type= "button">Finish</button></a>
+    <a href = "../Home/HomeWback.php"><button class = "btn-2" type= "button">Finish</button></a>
     </div>
-
+</div>
+</div>
+</div>
 </body>
 </html>
