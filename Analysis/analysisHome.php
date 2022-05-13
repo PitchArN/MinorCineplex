99a2294f037750695cb9547d0fa4a1696f368d18
PitchArN <73939643+PitchArN@users.noreply.github.com>
2022-05-14 02:55:20 +0700

    pre analys

---
+++ Analysis/analysisHome.php
@@ -0,0 +1,54 @@
+<?php
+	include 'connect.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>MinorCineplex | Analysis</title>
+<!---------- Boothstrap ------->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<!---------- Responsive ------->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!---------- CSS -------------->
+<link href = "../popcron/tien_style.css" rel="stylesheet"/> 
+
+</head>
+<body>
+	<div class="container bg-light ">
+		<div class="row">
+			<div class="col">
+				<h1>Advance Analysis</h1>
+			</div>
+
+			<div class="col">
+				<button class="btn-warning rounded-3">Movie Ticket Sales</button>
+			</div>	
+		</div>
+
+		<div class="row">
+			<div class="col">
+				<button class="btn-warning rounded-3">Movie Genre Sales</button>
+			</div>
+			
+			<div class="col">
+				<button class="btn-warning rounded-3">Product Sales</button>
+			</div>	
+		</div>
+
+		<div class="row">
+			<div class="col">
+				<button class="btn-warning rounded-3">Staff Working Hours</button>
+			</div>
+			
+			<div class="col">
+				<button class="btn-warning rounded-3">Promotion Usage</button>
+				
+			</div>	
+		</div>
+	</div>
+
+
+
+
+</body>
